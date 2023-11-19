a3a73f800581b2fee9ba01c9b5cf39dba9f04a64
naraekwon86 <wde0530@gmail.com>
2023-11-20 08:17:11 +0900

    board1

---
+++ src/main/java/com/ll/sb1120/Sb1120Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb1120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1120Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sb1120/Sb1120ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb1120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
